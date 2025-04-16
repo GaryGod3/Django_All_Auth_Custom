8476fe296a410a5ffb54fe074dd94ce203f87edb
Gary Godfrey <gary.god3@proton.me>
2025-04-16 10:04:12 +0100

    Update URL configuration comments and Django documentation link to version 5.2

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
-"""core URL Configuration
+"""
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,13 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from core import views
 
+
 urlpatterns = [
-    path('', include('allauth.urls')), # AllAuth Required NB. Not default of 'accounts/'
-    path('', include('your_app.urls')),
-    path('edit_user_details/', views.edit_user_details, name='edit_user_details'),
-    path('admin/', admin.site.urls),
+    path(
+        "", include("allauth.urls")
+    ),  # AllAuth Required NB. Not default of 'accounts/'
+    path("", include("your_app.urls")),
+    path("edit_user_details/", views.edit_user_details, name="edit_user_details"),
+    path("admin/", admin.site.urls),
 ]
