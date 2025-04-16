273094ac8b3cc6d2b73f4400188f6489ca56bf0a
Gary Godfrey <gary.god3@proton.me>
2025-04-16 10:02:33 +0100

    Refactor string quotes in index view for consistency

--- your_app/views.py
+++ your_app/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request, option='Home'):
+
+def index(request, option="Home"):
     """The home page for Your Site"""
     page_title = option
 
-    if option == 'Option3':
+    if option == "Option3":
         long_content = "Page filler for testing fixed navbar." * 80
     else:
         long_content = ""
 
     context = {
-        'page_title': page_title,
-        'long_content': long_content,
+        "page_title": page_title,
+        "long_content": long_content,
     }
-    return render(request, "index.html", context )
+    return render(request, "index.html", context)
